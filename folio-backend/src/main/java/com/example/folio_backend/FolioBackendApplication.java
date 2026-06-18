84d8d8bf52fe7cc2744635bea87541587cb62da9
Salma Ghrabi <salma.ghrabi@ensi-uma.tn>
2026-06-18 19:02:02 +0100

    chore: initialize backend project structure(#1)

---
+++ folio-backend/src/main/java/com/example/folio_backend/FolioBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.folio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolioBackendApplication.class, args);
+	}
+
+}
---
+++ folio-backend/src/test/java/com/example/folio_backend/FolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.folio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
