87529006be23c31ceb982ab6db7acb2f0c686483
Salma Ghrabi <salma.ghrabi@ensi-uma.tn>
2026-07-05 15:52:59 +0100

    feat(authentication): implement the User details Service

---
+++ folio-backend/src/main/java/com/example/folio_backend/security/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.folio_backend.security;
+
+import com.example.folio_backend.user.UserRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String useremail) throws UsernameNotFoundException {
+        return repository.findByEmail(useremail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
