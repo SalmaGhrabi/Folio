1d393b01fc5e22f7b2c6e30b0fd38b5a80ce5a3a
Salma Ghrabi <salma.ghrabi@ensi-uma.tn>
2026-07-26 18:13:40 +0100

    feat(authentication): create the Authentication Controller

---
+++ folio-backend/src/main/java/com/example/folio_backend/auth/AuthenticationService.java
@@ -0,0 +1,9 @@
+package com.example.folio_backend.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    public void register(RegistrationRequest request) {
+    }
+}
