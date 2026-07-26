5490494b0fb2d0d914c6bef295b4ff06a1129729
Salma Ghrabi <salma.ghrabi@ensi-uma.tn>
2026-07-26 18:30:45 +0100

    feat(authentication): implement the register method

--- folio-backend/src/main/java/com/example/folio_backend/auth/AuthenticationService.java
+++ folio-backend/src/main/java/com/example/folio_backend/auth/AuthenticationService.java
@@ -1,9 +1,69 @@
 package com.example.folio_backend.auth;
 
+import com.example.folio_backend.role.RoleRepossitory;
+import com.example.folio_backend.user.Token;
+import com.example.folio_backend.user.TokenRepository;
+import com.example.folio_backend.user.User;
+import com.example.folio_backend.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+
+    private final RoleRepossitory roleRepossitory;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
     public void register(RegistrationRequest request) {
+         var userRole = roleRepossitory.findByName("USER")
+                 // TODO- better exception HANDLING
+                 .orElseThrow(() -> new IllegalStateException("ROLE USER was not initialized"));
+         var user = User.builder()
+                 .firstname(request.getFirstname())
+                 .lastname(request.getLastname())
+                 .email(request.getEmail())
+                 .password(passwordEncoder.encode(request.getPassword()))
+                 .accountLocked(false)
+                 .enabled(false)
+                 .roles(List.of(userRole))
+                 .build();
+         userRepository.save(user);
+         sendValidationEmail(user);
+    }
+
+    private void sendValidationEmail(User user) {
+        var newToken = generateAndSaveActivationToken(user);
+    }
+
+    private String generateAndSaveActivationToken(User user) {
+        //generate token
+        String generatedToken = generateActivationCode(6);
+        var token = Token.builder()
+                .token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiresAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for  (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
     }
 }
