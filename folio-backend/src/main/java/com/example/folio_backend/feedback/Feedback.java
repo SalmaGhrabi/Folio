affa832cd0e7ea1fef7a757dc6f5fffffe5286ba
Salma Ghrabi <salma.ghrabi@ensi-uma.tn>
2026-08-08 12:32:03 +0100

    feat(book-entities): create the Feedback entity

--- folio-backend/src/main/java/com/example/folio_backend/feedback/Feedback.java
+++ folio-backend/src/main/java/com/example/folio_backend/feedback/Feedback.java
@@ -1,4 +1,34 @@
 package com.example.folio_backend.feedback;
 
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
 public class Feedback {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private Double note;
+    private String comment;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    @Column(insertable = false)
+    private LocalDateTime lastUpdatedDate;
+    @CreatedBy
+    @Column(updatable = false, nullable = false)
+    private Integer createdBy;
+    @LastModifiedBy
+    @Column(insertable = false)
+    private Integer lastUpdatedBy;
 }
