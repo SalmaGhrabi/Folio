0626627a23d119c7b089d2a8e373625da6794886
Salma Ghrabi <salma.ghrabi@ensi-uma.tn>
2026-08-10 21:31:40 +0100

    feat(book-management): implement the find book by id

--- folio-backend/src/main/java/com/example/folio_backend/book/BookService.java
+++ folio-backend/src/main/java/com/example/folio_backend/book/BookService.java
@@ -1,6 +1,7 @@
 package com.example.folio_backend.book;
 
 import com.example.folio_backend.user.User;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,11 @@ public class BookService {
         Book book = bookMapper.toBook(request);
         book.setOwner(user);
         return bookRepository.save(book).getId();
+    }
+
+    public BookResponse findById(Integer bookId) {
+        return bookRepository.findById(bookId)
+                .map(bookMapper::toBookResponse)
+                .orElseThrow(() -> new EntityNotFoundException("No book found with this ID : " + bookId));
     }
 }
